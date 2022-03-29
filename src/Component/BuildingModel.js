ee8edabea394060123c51313f27ab93d88dbb158
Pravin-kumar-25 <pravinkumar3725@gmail.com>
2022-03-30 01:27:26 +0530

    Remove unused imports

--- src/Component/BuildingModel.js
+++ src/Component/BuildingModel.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Building from './Building'
